786f415f02c4ecb21fd33fd65774731b02657bc0
ssianen <soph.sianen@gmail.com>
2024-08-23 15:01:50 -0700

    feat: working on player resizing, including regular size

--- Assets/Scripts/ResizePlayer.cs
+++ Assets/Scripts/ResizePlayer.cs
@@ -11,31 +11,67 @@ public class ResizePlayer : MonoBehaviour {
 
     public GameObject PotionIcon;
 
+    bool resized = false;
+    bool hasShrunk = false;
+
+    bool hasGrown = false;
+
+
+    Vector3 shrinkVector = new Vector3(0.5f, 0.5f, 0.5f);
+    Vector3 growVector = new Vector3(2.0f, 2.0f, 2.0f);
+    Vector3 regularVector = new Vector3(1.0f, 1.0f, 1.0f);
+
     void Start() {
+        
 
     }
 
     void Update() {
 
         if (CakeIcon.activeInHierarchy) {
-            if(Input.GetKey(KeyCode.E)){
-                //Grow the player
+            if(Input.GetKey(KeyCode.E) && resized == false && hasShrunk == false && hasGrown == false){
+                
+                //Grow from regular to large
                 transform.position = new Vector3 (transform.position.x, transform.position.y + 1.0f, transform.position.z);
-                transform.localScale = new Vector3 (2f, 2f, 2f);
+                transform.localScale = growVector;
+                
+                resized = true;
+                hasShrunk = false;
+                hasGrown = true;
+            } 
+
+            if (Input.GetKey(KeyCode.E) && resized == true && hasShrunk == false && hasGrown == true) {
+                
+                //Grow from small to regular 
+                transform.localScale = regularVector;
+                
+                hasShrunk = false;
+                hasGrown = true;
+                resized = false;
             }
 
         } 
 
         if (PotionIcon.activeInHierarchy) {
-            if(Input.GetKey(KeyCode.Q)){ 
-                //Shrink the player
+            if(Input.GetKey(KeyCode.Q) && resized == True && hasShrunk == false && hasGrown == true){ 
+                
+                //Shrink from large to regular
                 transform.position = new Vector3 (transform.position.x, transform.position.y + 0.5f, transform.position.z);
-                transform.localScale = new Vector3 (0.5f, 0.5f, 0.5f);
+                transform.localScale = shrinkVector;
+
+                hasShrunk = true;
+                resized = true;
+            } 
+            if (Input.GetKey(KeyCode.Q) && resized == false && hasShrunk == false && hasGrown == false) {
+                
+                //Shrink from regular to small 
+                transform.localScale = regularVector;
+                
+                hasShrunk = true;
+                hasGrown = false;
+                resized = false;
             }
         }
-
-        //yet to implement getting back to regular size
-        //transform.localScale = new Vector3 (1.0f, 1.0f, 1.0f);
 
     }
 
