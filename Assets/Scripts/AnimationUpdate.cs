5face3bc0e758cde2a566709efcf7b7b46ceab1c
AAllsopp <arianaraine@outlook.com>
2024-08-23 14:57:53 -0700

    feat: new animation options and jump
    Added animations (wip)

--- Assets/Scripts/AnimationUpdate.cs
+++ Assets/Scripts/AnimationUpdate.cs
@@ -21,11 +21,27 @@ public class AnimationUpdate : MonoBehaviour
         Vector3 moveDirectionVert = transform.forward * vertical;
         Vector3 newVelocity = moveDirectionVert * 2;
         // Animations
-        if(newVelocity == Vector3.zero){
+        
+
+        if(FindObjectOfType<PlayerMovememt>().GetVelocity() == Vector3.zero){
             animatorVal.SetFloat("Speed", 0);
         }
-        else {
+        else if (FindObjectOfType<PlayerMovememt>().GetSprint()){
+            animatorVal.SetFloat("Speed", 3);
+        } else if (FindObjectOfType<PlayerMovememt>().GetJump()) {
+            animatorVal.SetBool("Jump", true);
+        } else {
             animatorVal.SetFloat("Speed", 1);
+            animatorVal.SetBool("Jump", false);
         }
+
+        // if(newVelocity == Vector3.zero){
+        //     animatorVal.SetFloat("Speed", 0);
+        // }
+        // else if (Input.GetKey(KeyCode.LeftShift)){
+        //     animatorVal.SetFloat("Speed", 3);
+        // } else {
+        //     animatorVal.SetFloat("Speed", 1);
+        // }
     }
 }
