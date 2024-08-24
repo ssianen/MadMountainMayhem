398b5121a1c432114a8c3f34f161de6070ba84d7
ssianen <soph.sianen@gmail.com>
2024-08-23 19:37:45 -0700

    fix: resolved issue with alternating between resized to regular player sizes

--- Assets/Scripts/ResizePlayer.cs
+++ Assets/Scripts/ResizePlayer.cs
@@ -10,72 +10,101 @@ public class ResizePlayer : MonoBehaviour {
     public GameObject CakeIcon;
 
     public GameObject PotionIcon;
+    
 
-    bool resized = false;
-    bool hasShrunk = false;
+    private bool smallSized = false;
+    private bool regularSized = true;
 
-    bool hasGrown = false;
+    private bool largeSized = false;
 
 
-    Vector3 shrinkVector = new Vector3(0.5f, 0.5f, 0.5f);
-    Vector3 growVector = new Vector3(2.0f, 2.0f, 2.0f);
-    Vector3 regularVector = new Vector3(1.0f, 1.0f, 1.0f);
+    private Vector3 smallVector = new Vector3(0.5f, 0.5f, 0.5f);
+    private Vector3 regularVector = new Vector3(1.0f, 1.0f, 1.0f);
+    private Vector3 largeVector = new Vector3(2.0f, 2.0f, 2.0f);
 
-    void Start() {
-        
+    void Start() {   
 
     }
 
     void Update() {
 
-        if (CakeIcon.activeInHierarchy) {
-            if(Input.GetKey(KeyCode.E) && resized == false && hasShrunk == false && hasGrown == false){
+        //Growing (when just the cake is unlocked)
+        if (CakeIcon.activeInHierarchy && Input.GetKeyDown(KeyCode.E) && !PotionIcon.activeInHierarchy) {
+
+            if (regularSized) { 
                 
                 //Grow from regular to large
                 transform.position = new Vector3 (transform.position.x, transform.position.y + 1.0f, transform.position.z);
-                transform.localScale = growVector;
-                
-                resized = true;
-                hasShrunk = false;
-                hasGrown = true;
-            } 
+                transform.localScale = largeVector;
 
-            if (Input.GetKey(KeyCode.E) && resized == true && hasShrunk == false && hasGrown == true) {
-                
-                //Grow from small to regular 
-                transform.localScale = regularVector;
-                
-                hasShrunk = false;
-                hasGrown = true;
-                resized = false;
+                regularSized = false;
+                largeSized = true;
             }
-
         } 
+        
+        //Shrinking (when just the potion is unlocked)
+        else if (PotionIcon.activeInHierarchy && Input.GetKeyDown(KeyCode.Q) && !CakeIcon.activeInHierarchy) { 
 
-        if (PotionIcon.activeInHierarchy) {
-            if(Input.GetKey(KeyCode.Q) && resized == True && hasShrunk == false && hasGrown == true){ 
-                
-                //Shrink from large to regular
-                transform.position = new Vector3 (transform.position.x, transform.position.y + 0.5f, transform.position.z);
-                transform.localScale = shrinkVector;
-
-                hasShrunk = true;
-                resized = true;
-            } 
-            if (Input.GetKey(KeyCode.Q) && resized == false && hasShrunk == false && hasGrown == false) {
-                
+            if (regularSized) { 
+                    
                 //Shrink from regular to small 
-                transform.localScale = regularVector;
+                transform.position = new Vector3 (transform.position.x, transform.position.y + 0.5f, transform.position.z);
+                transform.localScale = smallVector;
+
+                regularSized = false;
+                smallSized = true;
+            } 
+
+        } else if (CakeIcon.activeInHierarchy && PotionIcon.activeInHierarchy){
+
+            if (Input.GetKeyDown(KeyCode.E)) {
                 
-                hasShrunk = true;
-                hasGrown = false;
-                resized = false;
-            }
+                if (regularSized) { 
+
+                    //Grow from regular to large
+                    transform.position = new Vector3 (transform.position.x, transform.position.y + 1.0f, transform.position.z);
+                    transform.localScale = largeVector;
+
+                    regularSized = false;
+                    largeSized = true;
+
+                } else if (smallSized) {
+
+                    //Grow from small to regular 
+                    transform.localScale = regularVector;
+                    
+                    smallSized = false;
+                    regularSized = true;
+
+                }
+
+            } else if (Input.GetKeyDown(KeyCode.Q)) {
+
+                if (regularSized) { 
+
+                    //Shrink from regular to small 
+                    transform.position = new Vector3 (transform.position.x, transform.position.y + 0.5f, transform.position.z);
+                    transform.localScale = smallVector;
+
+                    regularSized = false;
+                    smallSized = true;
+
+
+                } else if (largeSized) {
+
+                    //Shrink from small to regular 
+                    transform.localScale = regularVector;
+                    
+                    largeSized = false;
+                    regularSized = true;
+
+                }
+            }  
+
         }
-
     }
-
-   
 }
+
+
 
 
