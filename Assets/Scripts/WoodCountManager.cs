c3c5948823b8b8ac6046f7303bb691dd28d908eb
fireflylu <lu.school2023@gmail.com>
2024-08-28 12:29:03 -0700

    Fix: Axe put away after 20

--- Assets/Scripts/WoodCountManager.cs
+++ Assets/Scripts/WoodCountManager.cs
@@ -7,10 +7,14 @@ public class WoodCountManager : MonoBehaviour
 {
     public Text woodCountText;
     public static int totalChopped;
+    public GameObject axeOnPlayer;
 
     void Update(){
         woodCountText.text = totalChopped.ToString();
-    }
 
+	if (totalChopped == 20) {
+		axeOnPlayer.SetActive(false);
+	}
+    }
 
 }
